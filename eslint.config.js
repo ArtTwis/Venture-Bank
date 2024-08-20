c2adf34067e9c22afa353c196e56b82cdac3fee7
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-20 11:52:15 +0530

    modify eslint config file and husky pre-commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,31 @@
 import js from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default [
   js.configs.recommended,
   {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "prettier/prettier": "error",
     },
     ignores: ["node_modules", "dist", "build"],
+  },
+  {
+    settings: {
+      prettier: {
+        singleQuote: false,
+        bracketSpacing: true,
+        tabWidth: 2,
+        semi: true,
+        trailingComma: "es5",
+        bracketSameLine: false,
+        singleAttributePerLine: true,
+      },
+    },
   },
 ];
